d0c2cc73cf30edc2f3d506ec41cddc1bae551e83
gauravtripathii <gauravtripathii>
2024-02-06 01:18:39 +0530

    get user by id

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
-import { getAllUsers, createUser } from "../controllers/user.js";
+import { getAllUsers, createUser, getUserById } from "../controllers/user.js";
 
 import express from "express";
 const router = express.Router();
 
 router.get("/", getAllUsers);
+router.get("/:id", getUserById);
 router.post("/", createUser);
 
 export default router;
